ca6f4bad6f59e578315318fdd40dacaf95674639
Hamichev <60568270+Hamichev@users.noreply.github.com>
2020-03-20 20:29:30 +0300

    init:create code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,67 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr=expr.split(''),N="";
+  for (let i=0;i<arr.length;i++)
+  {
+    if (arr[i]==" ") {arr.splice(i,1);}
+  }
+  for (let i=0,min_number=0;i<arr.length;i++)
+  {
+    if (/\d/.test(arr[i])) 
+    {
+      min_number=i;
+      while (/\d/.test(arr[i]))
+      {
+        N+=arr[i];i++;
+      }
+      arr.splice(min_number+1,i-1-min_number);
+      i=min_number;
+      arr[i]=N;
+      N="";
+    }
+  }
+
+  for (let i=0,min_bracket=0,max_bracket=0;i<arr.length;i++)
+  {
+    if (arr[i]==")") 
+    {
+      max_bracket=i;
+      for (let j=i;j>0;j--)
+      {
+        if (arr[j]=="(") 
+        {
+          min_bracket=j;
+
+          for (let k=j,S=0;k<max_bracket;k++)
+          {
+            if (arr[k]=="*") {S=(+arr[k-1])*(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
+            if (arr[k]=="/") {S=(+arr[k-1])/(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
+          }
+          for (let k=j,S=0;k<max_bracket;k++)
+          {
+            if (arr[k]=="+") {S=(+arr[k-1])+(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
+            if (arr[k]=="-") {S=(+arr[k-1])-(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
+          }
+          break;
+        }
+      }
+      i=min_bracket;
+      arr[i]=arr[i+1];
+      arr.splice(i+1,2);
+    }
+  }
+  for (let i=0;i<arr.length;i++)
+  {
+    if (arr[i]=="*") {S=(+arr[i-1])*(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
+    if (arr[i]=="/") {S=(+arr[i-1])/(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
+  } 
+  for (let i=0;i<arr.length;i++)
+  {
+    if (arr[i]=="+") {S=(+arr[i-1])+(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
+    if (arr[i]=="-") {S=(+arr[i-1])-(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
+  }
+  return (+arr);
 }
 
 module.exports = {
