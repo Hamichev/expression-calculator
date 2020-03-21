d57e39392b360b6f6f8af4a6da4c931653faaa1a
Hamichev <60568270+Hamichev@users.noreply.github.com>
2020-03-21 09:14:59 +0300

    fix: Arbeiten

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ function eval() {
 
 function expressionCalculator(expr) {
     let arr=expr.split(''),N="";
+  for (let i=0,left_bracket=0,right_bracket=0;i<arr.length;i++)
+  {
+    if (arr[i]=="(") {left_bracket++;}
+    if (arr[i]==")") {right_bracket++;}
+    if (i==arr.length-1 && left_bracket!=right_bracket) {throw new Error('ExpressionError: Brackets must be paired');}
+  }
   for (let i=0;i<arr.length;i++)
   {
     if (arr[i]==" ") {arr.splice(i,1);}
@@ -30,7 +36,7 @@ function expressionCalculator(expr) {
     if (arr[i]==")") 
     {
       max_bracket=i;
-      for (let j=i;j>0;j--)
+      for (let j=i;j>-1;j--)
       {
         if (arr[j]=="(") 
         {
@@ -39,6 +45,7 @@ function expressionCalculator(expr) {
           for (let k=j,S=0;k<max_bracket;k++)
           {
             if (arr[k]=="*") {S=(+arr[k-1])*(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
+            if (arr[k]=="/" && arr[k+1]=="0") {throw new Error("TypeError: Division by zero.");}
             if (arr[k]=="/") {S=(+arr[k-1])/(+arr[k+1]);k-=1;arr[k]=S;arr.splice(k+1,2);max_bracket-=2;}
           }
           for (let k=j,S=0;k<max_bracket;k++)
@@ -50,13 +57,14 @@ function expressionCalculator(expr) {
         }
       }
       i=min_bracket;
-      arr[i]=arr[i+1];
-      arr.splice(i+1,2);
+      arr[i]=arr[i+2];
+      arr.splice(i+1,4);
     }
   }
   for (let i=0;i<arr.length;i++)
   {
     if (arr[i]=="*") {S=(+arr[i-1])*(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
+    if (arr[i]=="/" && arr[i+1]=="0") {throw new Error("TypeError: Division by zero.");}
     if (arr[i]=="/") {S=(+arr[i-1])/(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
   } 
   for (let i=0;i<arr.length;i++)
@@ -64,10 +72,10 @@ function expressionCalculator(expr) {
     if (arr[i]=="+") {S=(+arr[i-1])+(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
     if (arr[i]=="-") {S=(+arr[i-1])-(+arr[i+1]);i-=1;arr[i]=S;arr.splice(i+1,2);}
   }
-   let result=+arr.join('');
+  let result=+arr.join('');
    result= Math.round(result*10000);
    result/=10000;
-  return result;
+   return result;
 }
 
 module.exports = {
